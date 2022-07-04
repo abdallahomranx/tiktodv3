e5c7d3a1bd2e6dee3750c1ebdee2ec2aa2cb7f99
abdallahomranx <108660124+abdallahomranx@users.noreply.github.com>
2022-07-04 11:12:09 +0200

    Update tiktodv3.py

--- tiktodv3.py
+++ tiktodv3.py
@@ -25,7 +25,7 @@ print("1. Viewbot.\n2. Heartbot.\n3. Followerbot.\n3. Sharebot.\n4. Credits.\n")
 auto = int(input("Mode: "))
 
 if auto == 1 or auto == 2 or auto == 3 or auto == 4:
-    vidUrl = input("TikTok video URL: ")
+    vidUrl = input("TikTok video URL: <blockquote class="tiktok-embed" cite="https://www.tiktok.com/@abdallahomran" data-unique-id="abdallahomran"  data-embed-type="creator" style="max-width: 720px; min-width: 288px;" > <section> <a target="_blank" href="https://www.tiktok.com/@abdallahomran?refer=creator_embed">@abdallahomran</a> </section> </blockquote> <script async src="https://www.tiktok.com/embed.js"></script>")
 
     start = time()
     time_elapsed = strftime('%H:%M:%S', gmtime(time() - start))
@@ -37,9 +37,9 @@ if auto == 1 or auto == 2 or auto == 3 or auto == 4:
     driver = webdriver.Chrome( options=chrome_options)
     driver.set_window_size(1024, 650)
 
-    Views = 0
-    Hearts = 0
-    Followers = 0
+    Views = 1245675
+    Hearts = 24758
+    Followers = 124560
 
 def beautify(arg):
     return format(arg, ',d').replace(',', '.')
